5ba8e79ff495c1f65c0c5c3ca8c755668e7d4484
Francisco De La Cruz <helloquico@gmail.com>
2021-01-12 23:10:16 -0500

    Finish testing the blog w/Integration Tests

--- section3/blog/blog.py
+++ section3/blog/blog.py
@@ -1,3 +1,6 @@
+from post import Post
+
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -10,3 +13,12 @@ class Blog:
                                              len(self.posts),
                                              's' if len(self.posts) != 1 else '')
 
+    def create_post(self, title, content):
+        self.posts.append(Post(title, content))
+
+    def json(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts],
+        }
