8f8fe4008fda90be3739d694c4b1c9dfad035e47
Francisco De La Cruz <helloquico@gmail.com>
2021-01-13 21:17:07 -0500

    Patch the input method

--- section3/blog/tests/system/test_app.py
+++ section3/blog/tests/system/test_app.py
@@ -5,9 +5,23 @@ from blog import Blog
 
 
 class TestApp(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input'):
+                app.menu()
+
+                mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog('Test Blog', 'Test Author')
         app.blogs = {'Test Blog': blog}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
+
             mocked_print.assert_called_with('- Test Blog by Test Author (0 posts)')
