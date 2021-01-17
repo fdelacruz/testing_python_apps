8123129e8c1ccc062f0682cdf724c9bd4bb52704
Francisco De La Cruz <helloquico@gmail.com>
2021-01-17 09:22:25 -0500

    Unit testing our REST API

--- section5/models/item.py
+++ section5/models/item.py
@@ -1,4 +1,4 @@
-from db import db
+from section5.db import db
 
 
 class ItemModel(db.Model):
---
+++ section5/tests/unit/models/test_item.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from section5.models.item import ItemModel
+
+
+class TestItem(TestCase):
+    def test_create_item(self):
+        item = ItemModel('test', 0.99)
+
+        self.assertEqual(item.name, 'test',
+                         "The name of the item after creation does not equal the constructor argument.")
+        self.assertEqual(item.price, 0.99,
+                         "The price of the item after creation does not equal the constructor argument.")
+
+    def test_item_json(self):
+        item = ItemModel('test', 0.99)
+
+        expected = {
+            'name': item.name,
+            'price': 0.99
+        }
+
+        self.assertEqual(expected, item.json(), "The JSON export of the item is incorrect. Expected {}, received {}.".format(expected, item.json()))
