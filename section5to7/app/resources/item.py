fb260a0b7cd1e5a28426e7af6333fb136d9617de
Francisco De La Cruz <helloquico@gmail.com>
2021-01-18 22:06:30 -0500

    REST API Testing, Part II

--- section5to7/app/resources/item.py
+++ section5to7/app/resources/item.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
 from models.item import ItemModel
 
 
@@ -8,7 +9,12 @@ class Item(Resource):
                         type=float,
                         required=True,
                         help="This field cannot be left blank!")
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id.")
 
+    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -54,4 +60,4 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items': [x.json() for x in ItemModel.query.all()]}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
--- section5to7/app/tests/base_test.py
+++ section5to7/app/tests/base_test.py
@@ -5,6 +5,7 @@ This class should be the parent class to each non-unit test.
 It allows for instantiation of the database dynamically and makes sure
 that it is a new, blank database each time.
 """
+
 from unittest import TestCase
 from app import app
 from db import db
