bf1c331bd839f468903b0c712bae36a7c2258077
Francisco De La Cruz <helloquico@gmail.com>
2021-01-18 22:37:52 -0500

    Test foreign key constraints

--- section5to7/app/tests/integration/models/test_item.py
+++ section5to7/app/tests/integration/models/test_item.py
@@ -5,7 +5,7 @@ from tests.base_test import BaseTest
 class TestItem(BaseTest):
     def test_crud(self):
         with self.app_context():
-            item = ItemModel('test', 0.99)
+            item = ItemModel('test', 0.99, 1)
 
             self.assertIsNone(ItemModel.find_by_name('test'),
                               f"Found an item with name {item.name}, but expected not too.")
