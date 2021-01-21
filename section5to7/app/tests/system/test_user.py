7f657d7d6836507ec3005b431fa7b0f0db92dca5
Francisco De La Cruz <helloquico@gmail.com>
2021-01-21 08:29:19 -0500

    Finalise the user Systems tests

--- section5to7/app/tests/system/test_user.py
+++ section5to7/app/tests/system/test_user.py
@@ -7,15 +7,29 @@ class TestUser(BaseTest):
     def test_register_user(self):
         with self.app() as client:
             with self.app_context():
-                request = client.post("/register", data={'username': 'test', 'password': '1234'})
+                response = client.post("/register", data={'username': 'test', 'password': '1234'})
 
-                self.assertEqual(request.status_code, 200)
+                self.assertEqual(response.status_code, 200)
                 self.assertIsNotNone(UserModel.find_by_name('test'))
                 self.assertDictEqual({'message': 'User created successfully.'},
-                                     json.loads(request.data))
+                                     json.loads(response.data))
 
     def test_register_and_login(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post("/register", data={'username': 'test', 'password': '1234'})
+                auth_response = client.post('/auth',
+                                           data=json.dumps({'username': 'test',
+                                                            'password': '1234'}),
+                                           headers={'Content-Type': 'application/json'})
+
+                self.assertIn('access_token', json.loads(auth_response.data).keys())
 
     def test_register_duplicate_user(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post("/register", data={'username': 'test', 'password': '1234'})
+                response = client.post("/register", data={'username': 'test', 'password': '1234'})
+
+                self.assertEqual(response.status_code, 400)
+                self.assertDictEqual({'message': 'A user with that username already exists.'}, json.loads(response.data))
